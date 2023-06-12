1a491e9d85eec34b8f88de0d01e370a59281df96
Kevin Sullivan <kevin@sull.vn>
2023-06-12 16:53:13 -0400

    Parse arguments

--- Makefile
+++ Makefile
@@ -5,22 +5,24 @@ all: a.out
 clean:
 	rm -f a.out iostream.pcm
 
-CPP := clang++ \
-	-std=c++2b \
-	-Weverything \
-	-Wno-c++98-compat
 
-CPP_SYSTEM_HEADER := $(CPP) \
-	-Wno-reserved-macro-identifier \
-	-Wno-pragma-system-header-outside-header \
+CPP_BASE := clang++ \
+	-std=c++2b \
+
+CPP := $(CPP_BASE) \
+	-Weverything \
+	-Wno-c++98-compat-pedantic
+
+a.out: main.cpp
+	$(CPP) \
+		main.cpp
+
+
+CPP_SYSTEM_HEADER := $(CPP_BASE) \
+	-Wno-everything \
 	-I/usr/local/opt/llvm/include \
 	-I/usr/local/opt/llvm/include/c++/v1/ \
-	-xc++-system-header \
-
-a.out: main.cpp iostream.pcm
-	$(CPP) \
-		-fmodule-file=iostream.pcm \
-		main.cpp
+	-xc++-system-header
 
 iostream.pcm:
 	$(CPP_SYSTEM_HEADER) \
--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
-import <iostream>;
+#include <cstddef>
+#include <iostream>
+#include <optional>
+#include <ranges>
+#include <span>
+#include <string>
+#include <stdexcept>
 
-using namespace std;
+struct Config {
+  std::size_t number;
+};
 
-auto main() -> int {
-  cout << "Hello";
+constexpr auto parse_args(const std::span<char *>& args) -> std::optional<Config> {
+  if (args.size() != 2) {
+    return {};
+  }
+
+  auto number_str = args[1];
+  std::size_t number{};
+
+  try {
+    number = std::stoull(number_str);
+  } catch (const std::invalid_argument&) {
+    return {};
+  }
+
+  return Config{.number = number};
 }
+
+auto main(int argc, char *argv[]) -> int {
+  std::span args{argv, static_cast<std::size_t>(argc)};
+  auto maybe_config = parse_args(args);
+  if (!maybe_config.has_value()) {
+    std::cout << "USAGE: a.out <number>\n";
+    return 1;
+  }
+
+  auto config = maybe_config.value();
+
+  for (auto i : std::ranges::views::iota(0ull, config.number)) {
+    std::cout << i << "\n";
+  }
+}
+
